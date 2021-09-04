19c588ccf2acc656bd2d159ff88a033b8f2996ac
Steve Yeager <steve.f.yeager@gmail.com>
2021-09-03 18:07:11 -0700

    Add DiagnosticContext and split up files

---
+++ LittleByte.Logging/Logs.cs
@@ -0,0 +1,15 @@
+﻿using Serilog;
+
+namespace LittleByte.Logging
+{
+    public sealed class NullDiagnosticContext : IDiagnosticContext
+    {
+        public void Set(string propertyName, object value, bool destructureObjects = false) { }
+    }
+
+    public static class Logs
+    {
+        public static ILog Props() => new Log();
+        public static IDiagnosticContext DiagnosticContext { get; set; } = new NullDiagnosticContext();
+    }
+}
