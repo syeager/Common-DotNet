2591607a7f793d5b9cef15768306b350a9968bd0
Steve <steve.f.yeager@gmail.com>
2021-10-16 07:52:35 -0700

    Add EmptyType
    Utility type to improve code readability by removing arbitrary type assignments.

---
+++ LittleByte.Core/Common/EmptyType.cs
@@ -0,0 +1,7 @@
+﻿namespace LittleByte
+{
+    /// <summary>
+    /// Only used for shortening lines when a generic type is necessary for the compiler but the code doesn't require it (eg: nameof, accessing static members, unit tests).
+    /// </summary>
+    public sealed class X { }
+}
