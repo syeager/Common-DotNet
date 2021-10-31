d12eb873aef7be0435b6acc1e016ba763a07b78b
Steve <steve.f.yeager@gmail.com>
2021-10-31 09:08:04 -0700

    Add EntityIdCache to Infra

---
+++ LittleByte.Infra/Services/EntityIdCache.cs
@@ -0,0 +1,34 @@
+﻿using LittleByte.Infra.Models;
+using Serilog;
+
+namespace Articlib.Articles.Infra;
+
+public interface IEntityIdReadCache
+{
+    public Guid Get(string identifier);
+}
+
+public interface IEntityIdWriteCache : IEntityIdReadCache
+{
+    public void Add(IEntity entity);
+    public void Add(string identifier, Guid id);
+}
+
+public class EntityIdCache : IEntityIdWriteCache
+{
+    private readonly ILogger logger = Log.ForContext<EntityIdCache>();
+    private readonly Dictionary<string, Guid> cache = new();
+
+    public void Add(IEntity entity) => Add(entity.Identifier, entity.Id);
+
+    public void Add(string identifier, Guid id)
+    {
+        logger
+            .ForContext("Entity.Id", id)
+            .ForContext("Entity.Identifier", identifier)
+            .Debug("Cache entry Id");
+        cache[identifier] = id;
+    }
+
+    public Guid Get(string identifier) => cache[identifier];
+}
