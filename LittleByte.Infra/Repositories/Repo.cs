ff02323bfe2603d2150b6957b541c0432a9f2d9d
Steve Yeager <steve.f.yeager@gmail.com>
2021-11-02 22:21:20 -0700

    Add Repo class

---
+++ LittleByte.Infra/Repositories/Repo.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+namespace LittleByte.Infra;
+
+public interface IRepo
+{
+    public Task<int> SaveChangesAsync();
+}
+
+public class Repo<TContext> : IRepo where TContext : DbContext
+{
+    protected readonly ILogger logger;
+    protected readonly TContext dbContext;
+    protected readonly IMapper mapper;
+    protected readonly IEntityIdWriteCache modelIdCache;
+
+    public Repo(TContext dbContext, IMapper mapper, IEntityIdWriteCache modelIdCache)
+    {
+        logger = Log.ForContext(GetType());
+        this.dbContext = dbContext;
+        this.mapper = mapper;
+        this.modelIdCache = modelIdCache;
+    }
+
+    public virtual Task<int> SaveChangesAsync() => dbContext.SaveChangesAsync();
+}
