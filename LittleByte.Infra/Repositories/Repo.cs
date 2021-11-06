8e1818bdd9ba9df452ed7c7268198296fb716c37
Steve Yeager <steve.f.yeager@gmail.com>
2021-11-06 11:53:58 -0700

    Remove EntityModelIdCache from Repo

--- LittleByte.Infra/Repositories/Repo.cs
+++ LittleByte.Infra/Repositories/Repo.cs
@@ -14,14 +14,12 @@ public class Repo<TContext> : IRepo where TContext : DbContext
     protected readonly ILogger logger;
     protected readonly TContext dbContext;
     protected readonly IMapper mapper;
-    protected readonly IEntityIdWriteCache modelIdCache;
 
-    public Repo(TContext dbContext, IMapper mapper, IEntityIdWriteCache modelIdCache)
+    public Repo(TContext dbContext, IMapper mapper)
     {
         logger = Log.ForContext(GetType());
         this.dbContext = dbContext;
         this.mapper = mapper;
-        this.modelIdCache = modelIdCache;
     }
 
     public virtual Task<int> SaveChangesAsync() => dbContext.SaveChangesAsync();
