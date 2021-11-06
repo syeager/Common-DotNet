85032379f4dde4dcf98e15f276c86140d01a9f08
Steve Yeager <steve.f.yeager@gmail.com>
2021-11-06 16:43:59 -0700

    Improve valid model validation messages

--- LittleByte.Validation.Test/TestUtilities/ValidModelExtension.cs
+++ LittleByte.Validation.Test/TestUtilities/ValidModelExtension.cs
@@ -6,10 +6,10 @@ namespace LittleByte.Validation.Test.TestUtilities
     {
         public static void AssertFirstError<T>(this Valid<T> @this, string propertyName, string errorCode) where T : class
         {
-            Assert.IsFalse(@this.IsSuccess);
+            Assert.IsFalse(@this.IsSuccess, "Expected failed validation");
             var error = @this.Validation.Errors[0];
-            Assert.AreEqual(propertyName, error.PropertyName);
-            Assert.AreEqual(errorCode, error.ErrorCode);
+            Assert.AreEqual(propertyName, error.PropertyName, "Incorrect property name");
+            Assert.AreEqual(errorCode, error.ErrorCode, "Incorrect error code");
         }
     }
 }
