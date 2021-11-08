ea7df675733161f0831e8863284964861ae73137
Steve Yeager <steve.f.yeager@gmail.com>
2021-11-07 22:04:59 -0800

    Add extension methods for mocking validation pass and fail

---
+++ LittleByte.Validation.Test/TestUtilities/IModelValidatorExtension.cs
@@ -0,0 +1,16 @@
+﻿using NSubstitute;
+
+namespace LittleByte.Validation.Test.TestUtilities;
+
+public static class IModelValidatorExtension
+{
+    public static void Fail<T>(this IModelValidator<T> @this) where T : class
+    {
+        @this.Sign(Arg.Any<T>()).ReturnsForAnyArgs(ValidModel.Failed<T>());
+    }
+
+    public static void Pass<T>(this IModelValidator<T> @this) where T : class
+    {
+        @this.Sign(Arg.Any<T>()).ReturnsForAnyArgs(ValidModel.Succeeded<T>());
+    }
+}
