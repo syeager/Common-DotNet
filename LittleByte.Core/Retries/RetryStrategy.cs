cb8749d070f1f9b7da5fbd370bf98f8f93e8814d
Steve <steve.f.yeager@gmail.com>
2021-11-28 14:01:05 -0800

    Add retry strategies

---
+++ LittleByte.Core/Retries/RetryStrategy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LittleByte.Core;
+
+public record RetrySettings(int Attempts = 3, int DelayMs = 3000);
+
+public abstract class RetryStrategy
+{
+    protected abstract ValueTask RunAsyncInternal(Action action, RetrySettings settings);
+    protected abstract ValueTask<T?> RunAsyncInternal<T>(Func<T> action, RetrySettings settings) where T : class;
+
+    public ValueTask RunAsync(Action action, RetrySettings? settings = null)
+    {
+        settings ??= new RetrySettings();
+        return RunAsyncInternal(action, settings);
+    }
+
+    public ValueTask<T?> RunAsync<T>(Func<T> action, RetrySettings? settings = null) where T : class
+    {
+        settings ??= new RetrySettings();
+        return RunAsyncInternal(action, settings);
+    }
+}
