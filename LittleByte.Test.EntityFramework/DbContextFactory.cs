29a094d607fbc0987174fecf889bbceaf5a9f536
Steve <steve.f.yeager@gmail.com>
2021-12-23 19:42:32 -0800

    Add LittleByte.Test.EntityFramework project

---
+++ LittleByte.Test.EntityFramework/DbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace LittleByte.Test.EntityFramework;
+
+public static class DbContextFactory
+{
+    public static void InMemory<T>(ref T? context)
+        where T : DbContext
+    {
+        if (context == null)
+        {
+            var options = new DbContextOptionsBuilder<T>()
+                .UseInMemoryDatabase(typeof(T).Name)
+                .Options;
+
+            context = (T) Activator.CreateInstance(typeof(T), options)!;
+        }
+        
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+    }
+}
