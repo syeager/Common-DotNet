66aa935ae14e03ef3ea44980150a20df0a69368d
Steve <steve.f.yeager@gmail.com>
2021-12-23 20:11:50 -0800

    Add MapRange IMapper extension

---
+++ LittleByte.Extensions.AutoMapper/IMapperExtension.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace LittleByte.Extensions.AutoMapper;
+
+public static class IMapperExtension
+{
+    public static T[] MapRange<T>(this IMapper @this, IEnumerable<object> source)
+    {
+        return source.Select(@this.Map<T>).ToArray();
+    }
+}
