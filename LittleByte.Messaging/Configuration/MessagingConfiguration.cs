e9f705f122f19c9646bb41d7c409bd7bf261e2a6
Steve Yeager <steve.f.yeager@gmail.com>
2021-12-30 21:17:03 -0800

    Move messaging configuration to LittleByte.Messaging project

---
+++ LittleByte.Messaging/Configuration/MessagingConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using LittleByte.Configuration;
+using LittleByte.Core.Extensions;
+using LittleByte.Messaging.RabbitMq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LittleByte.Messaging.Configuration;
+
+public static class MessagingConfiguration
+{
+    public static IServiceCollection AddMessaging(this IServiceCollection @this, IConfiguration configuration)
+    {
+        @this.BindOptions<RabbitMqOptions>(configuration);
+
+        return @this
+            .AddSingleton<IMessageSerializer, JsonTextSerializer>()
+            .AddHostedService<MessagePublisher, RabbitMqPublisher>();
+    }
+}
