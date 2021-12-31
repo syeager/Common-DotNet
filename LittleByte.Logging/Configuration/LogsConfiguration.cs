e329d1683039f2834a0d8273b5f24b06bee0e795
Steve Yeager <steve.f.yeager@gmail.com>
2021-12-30 21:16:47 -0800

    Move logging configuration to LittleByte.Logging project

---
+++ LittleByte.Logging/Configuration/LogsConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace LittleByte.Logging.Configuration;
+
+public static class LogsConfiguration
+{
+    public static void AddLogs(this IServiceCollection services)
+    {
+        using var serviceProvider = services.BuildServiceProvider();
+        Logs.DiagnosticContext = serviceProvider.GetRequiredService<IDiagnosticContext>();
+    }
+}
