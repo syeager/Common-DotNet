048ef543b12d21c570c139ab0e85a5fd32f30f4f
Steve <steve.f.yeager@gmail.com>
2022-01-04 18:53:34 -0800

    Add detailed logs option to mysql

--- LittleByte.Extensions.Pomelo.EntityFrameworkCore.MySql/MySqlConfiguration.cs
+++ LittleByte.Extensions.Pomelo.EntityFrameworkCore.MySql/MySqlConfiguration.cs
@@ -10,11 +10,18 @@ public static class PersistenceConfiguration
     public static IServiceCollection AddMySql<TContext>(this IServiceCollection @this, IConfiguration configuration)
         where TContext : DbContext
     {
-        var (connectionString, version) = configuration.GetSection<MySqlOptions>();
+        var (connectionString, version, detailedLogs) = configuration.GetSection<MySqlOptions>();
         var serverVersion = ServerVersion.Parse(version);
 
-        return @this.AddDbContext<TContext>(builder => builder.UseMySql(connectionString, serverVersion)
-            .EnableDetailedErrors()
-            .EnableSensitiveDataLogging());
+        return @this.AddDbContext<TContext>(builder =>
+        {
+            builder.UseMySql(connectionString, serverVersion);
+            if(detailedLogs)
+            {
+                builder
+                    .EnableDetailedErrors()
+                    .EnableSensitiveDataLogging();
+            }
+        });
     }
 }
--- LittleByte.Extensions.Pomelo.EntityFrameworkCore.MySql/MySqlOptions.cs
+++ LittleByte.Extensions.Pomelo.EntityFrameworkCore.MySql/MySqlOptions.cs
@@ -7,10 +7,12 @@ internal class MySqlOptions
 {
     public string ConnectionString { get; init; } = null!;
     public string Version { get; init; } = null!;
+    public bool DetailedLogs { get; init; }
 
-    public void Deconstruct(out string connectionString, out string version)
+    public void Deconstruct(out string connectionString, out string version, out bool detailedLogs)
     {
         connectionString = ConnectionString;
         version = Version;
+        detailedLogs = DetailedLogs;
     }
 }
