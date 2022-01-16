2689b0b2e2b5aec39351d89b4c3a81f1fc4dc98a
Steve Yeager <steve.f.yeager@gmail.com>
2022-01-16 13:33:19 -0800

    Return IServiceCollection from AddLogs

--- LittleByte.Logging/Configuration/LogsConfiguration.cs
+++ LittleByte.Logging/Configuration/LogsConfiguration.cs
@@ -1,13 +1,14 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 
 namespace LittleByte.Logging.Configuration;
 
 public static class LogsConfiguration
 {
-    public static void AddLogs(this IServiceCollection services)
+    public static IServiceCollection AddLogs(this IServiceCollection services)
     {
         using var serviceProvider = services.BuildServiceProvider();
         Logs.DiagnosticContext = serviceProvider.GetRequiredService<IDiagnosticContext>();
+        return services;
     }
 }
