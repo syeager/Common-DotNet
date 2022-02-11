98b752e8f0590673a3e5cd54fe53d8d7fb72bfbe
Steve <steve.f.yeager@gmail.com>
2022-02-10 20:25:20 -0800

    Add Profile.CreateBiDirectionMap

---
+++ LittleByte.Extensions.AutoMapper/ProfileExtension.cs
@@ -0,0 +1,12 @@
+using AutoMapper;
+
+namespace LittleByte.Extensions.AutoMapper;
+
+public static class ProfileExtension
+{
+    public static void CreateBiDirectionMap<T1, T2>(this Profile @this)
+    {
+        @this.CreateMap<T1, T2>();
+        @this.CreateMap<T2, T1>();
+    }
+}
