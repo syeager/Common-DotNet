f0e1dc87cf4dc42b09ee01fb97c64b7ac5d4c7b4
Steve <steve.f.yeager@gmail.com>
2022-08-26 10:05:06 -0700

    Add common endpoint names

---
+++ LittleByte.Extensions.AspNet/Core/Routes.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace LittleByte.Extensions.AspNet.Core;
+
+[SuppressMessage("ReSharper", "PartialTypeWithSinglePart")]
+public static partial class Routes
+{
+    public const string GetById = "get-id";
+    public const string GetByPage = "get-page";
+}
