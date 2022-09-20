119adde6d514f28401792912fca4c329ec5440b1
Steve Yeager <steve.f.yeager@gmail.com>
2022-09-20 10:10:18 -0700

    Add non-generic OkResponse

--- LittleByte.Common/AspNet/Responses/OkResponse.cs
+++ LittleByte.Common/AspNet/Responses/OkResponse.cs
@@ -2,6 +2,14 @@
 
 namespace LittleByte.Common.AspNet.Responses
 {
+    public class OkResponse : ApiResponse
+    {
+        public OkResponse(HttpStatusCode statusCode, string? message = null)
+            : base(statusCode, message)
+        {
+        }
+    }
+
     public class OkResponse<T> : ApiResponse<T> where T : class
     {
         public OkResponse(T obj, string? message = null)
