602407e3965883fbd001b1036d087cafecda3b5d
Steve <steve.f.yeager@gmail.com>
2022-10-03 16:59:46 -0400

    Add Routes.Create

--- LittleByte.Extensions.AspNet/Core/Routes.cs
+++ LittleByte.Extensions.AspNet/Core/Routes.cs
@@ -5,6 +5,7 @@ namespace LittleByte.Extensions.AspNet.Core;
 [SuppressMessage("ReSharper", "PartialTypeWithSinglePart")]
 public static partial class Routes
 {
+    public const string Create = "create";
     public const string GetById = "get-id";
     public const string GetByPage = "get-page";
 }
