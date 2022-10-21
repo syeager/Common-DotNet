c29936ed8509a5040a59f5613eed459bb02bfd52
Steve Yeager <steve.f.yeager@gmail.com>
2022-10-20 19:10:49 -0700

    Add new MapBoth overload

--- LittleByte.Common/AspNet/AutoMapper/ProfileExtension.cs
+++ LittleByte.Common/AspNet/AutoMapper/ProfileExtension.cs
@@ -4,9 +4,16 @@ namespace LittleByte.Common.AspNet.AutoMapper;
 
 public static class ProfileExtension
 {
-    public static void CreateBiDirectionMap<T1, T2>(this Profile @this)
+    public static void MapBoth<T1, T2>(this Profile @this)
     {
         @this.CreateMap<T1, T2>();
         @this.CreateMap<T2, T1>();
+    }
+
+    public static void MapBoth<T1, T2, TMap>(this Profile @this)
+        where TMap : ITypeConverter<T1, T2>, ITypeConverter<T2, T1>
+    {
+        @this.CreateMap<T1, T2>().ConvertUsing<TMap>();
+        @this.CreateMap<T2, T1>().ConvertUsing<TMap>();
     }
 }
