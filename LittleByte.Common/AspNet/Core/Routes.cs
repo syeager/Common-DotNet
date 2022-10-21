2f4848e14dd24111751fb44152d978aedf72f767
Steve Yeager <steve.f.yeager@gmail.com>
2022-10-20 21:07:43 -0700

    Add Routes.LogIn

--- LittleByte.Common/AspNet/Core/Routes.cs
+++ LittleByte.Common/AspNet/Core/Routes.cs
@@ -8,4 +8,5 @@ public static partial class Routes
     public const string Create = "create";
     public const string GetById = "get-id";
     public const string GetByPage = "get-page";
+    public const string LogIn = "log-in";
 }
