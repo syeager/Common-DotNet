c679d83ffd83d6bea331a8a8e83851bea7ba6a9d
Steve Yeager <steve.f.yeager@gmail.com>
2022-10-22 05:39:33 -0700

    Add Jwt to string map

---
+++ LittleByte.Common/Identity/Mappings/IdentityProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using JetBrains.Annotations;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace LittleByte.Common.Identity.Mappings;
+
+[UsedImplicitly]
+internal sealed class IdentityProfile : Profile
+{
+    public IdentityProfile()
+    {
+        CreateMap<JwtSecurityToken, string>().ConvertUsing<JwtSecurityTokenConverter>();
+    }
+}
---
+++ LittleByte.Common/Identity/Mappings/JwtSecurityTokenConverter.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics.CodeAnalysis;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace LittleByte.Common.Identity.Mappings;
+
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+public sealed class JwtSecurityTokenConverter : ITypeConverter<JwtSecurityToken, string>
+{
+    private readonly SecurityTokenHandler tokenHandler;
+
+    public JwtSecurityTokenConverter(SecurityTokenHandler tokenHandler)
+    {
+        this.tokenHandler = tokenHandler;
+    }
+
+    public string Convert(JwtSecurityToken source, string destination, ResolutionContext context) =>
+        tokenHandler.WriteToken(source);
+}
