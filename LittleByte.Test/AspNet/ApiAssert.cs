6f638e421f42e29fcd9d9169e4f4bd36b217d5d5
Steve Yeager <steve.f.yeager@gmail.com>
2022-10-31 11:31:11 -0700

    Add ApiAssert to asset with integration tests

---
+++ LittleByte.Test/AspNet/ApiAssert.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using LittleByte.Common.AspNet.Responses;
+using NUnit.Framework;
+
+namespace LittleByte.Test.AspNet;
+
+public static class ApiAssert
+{
+    public static void IsSuccess<T>(ApiResponse<T> response, HttpStatusCode expectedCode = HttpStatusCode.OK)
+        where T : class
+    {
+        Assert.IsFalse(response.IsError);
+        Assert.IsNotNull(response.Obj);
+        Assert.AreEqual((int)expectedCode, response.StatusCode);
+    }
+
+    public static void IsFailure(ApiResponse response, HttpStatusCode expectedCode = HttpStatusCode.BadRequest)
+    {
+        Assert.IsTrue(response.IsError);
+        Assert.AreEqual((int)expectedCode, response.StatusCode);
+    }
+}
