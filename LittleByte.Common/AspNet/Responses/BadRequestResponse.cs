9c3ef35842a46de1b709523928b00ede126da96c
Steve Yeager <steve.f.yeager@gmail.com>
2022-10-31 11:30:06 -0700

    Add BadRequestResponse

---
+++ LittleByte.Common/AspNet/Responses/BadRequestResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace LittleByte.Common.AspNet.Responses;
+
+public class BadRequestResponse : ApiResponse
+{
+    public BadRequestResponse(string? message = null) : base(HttpStatusCode.BadRequest, message) { }
+}
+
+public class BadRequestResponse<T> : ApiResponse<T> where T : class
+{
+    public BadRequestResponse(T? obj, string? message = null) : base(HttpStatusCode.BadRequest, obj, message) { }
+    public BadRequestResponse(string message) : base(HttpStatusCode.BadRequest, message) { }
+}
