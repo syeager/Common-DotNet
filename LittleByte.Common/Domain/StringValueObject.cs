650515652b995892d78a51506fa6d285168bd355
Steve Yeager <steve.f.yeager@gmail.com>
2022-11-04 10:46:41 -0700

    Add StringValueObject

---
+++ LittleByte.Common/Domain/StringValueObject.cs
@@ -0,0 +1,8 @@
+﻿namespace LittleByte.Common.Domain;
+
+public abstract record StringValueObject(string Value)
+{
+    public override string ToString() => Value;
+
+    public static implicit operator string(StringValueObject @this) => @this.Value;
+}
