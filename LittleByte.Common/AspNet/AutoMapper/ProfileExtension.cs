b42b90c5c7475325abee6888a4c965c42af1868c
Steve Yeager <steve.f.yeager@gmail.com>
2022-11-06 05:52:36 -0800

    Change mapping to include method for converting both or just one way

--- LittleByte.Common/AspNet/AutoMapper/ProfileExtension.cs
+++ LittleByte.Common/AspNet/AutoMapper/ProfileExtension.cs
@@ -10,10 +10,17 @@ public static class ProfileExtension
         @this.CreateMap<T2, T1>();
     }
 
-    public static void MapBoth<T1, T2, TMap>(this Profile @this)
+    public static void MapBothConvertBoth<T1, T2, TMap>(this Profile @this)
         where TMap : ITypeConverter<T1, T2>, ITypeConverter<T2, T1>
     {
         @this.CreateMap<T1, T2>().ConvertUsing<TMap>();
         @this.CreateMap<T2, T1>().ConvertUsing<TMap>();
+    }
+    
+    public static void MapBothConvertOne<T1, T2, TMap>(this Profile @this)
+        where TMap : ITypeConverter<T1, T2>
+    {
+        @this.CreateMap<T1, T2>().ConvertUsing<TMap>();
+        @this.CreateMap<T2, T1>();
     }
 }
