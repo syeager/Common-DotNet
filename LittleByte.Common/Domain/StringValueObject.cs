1a12f299ed08d153ecaa0cc8c566dd3f13eaaa09
Steve <steve.f.yeager@gmail.com>
2023-01-21 13:19:33 -0800

    Add StringValueObjectConverter

--- LittleByte.Common/Domain/StringValueObject.cs
+++ LittleByte.Common/Domain/StringValueObject.cs
@@ -1,8 +1,15 @@
-﻿namespace LittleByte.Common.Domain;
+﻿using AutoMapper;
+
+namespace LittleByte.Common.Domain;
 
 public abstract record StringValueObject(string Value)
 {
     public override string ToString() => Value;
 
     public static implicit operator string(StringValueObject @this) => @this.Value;
+}
+
+public sealed class StringValueObjectConverter : ITypeConverter<StringValueObject, string>
+{
+    public string Convert(StringValueObject source, string destination, ResolutionContext context) => source.Value;
 }
