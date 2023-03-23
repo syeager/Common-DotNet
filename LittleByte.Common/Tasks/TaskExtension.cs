db1c8d590e258c9f8b87908750ebd3eaa27f741f
Steve Yeager <steve.f.yeager@gmail.com>
2023-03-22 20:01:23 -0700

    Ad NoAwait Task and ValueTask extension methods

---
+++ LittleByte.Common/Tasks/TaskExtension.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace LittleByte.Common.Tasks;
+
+public static class TaskExtension
+{
+    public static ConfiguredTaskAwaitable<T> NoAwait<T>(this Task<T> @this) => @this.ConfigureAwait(false);
+}
---
+++ LittleByte.Common/Tasks/ValueTaskExtension.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace LittleByte.Common.Tasks;
+
+public static class ValueTaskExtension
+{
+    public static ConfiguredValueTaskAwaitable<T> NoAwait<T>(this ValueTask<T> @this) => @this.ConfigureAwait(false);
+}
