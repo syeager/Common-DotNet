741d03b03d5f7d4e0830f48ae48216b5e012c6ce
Steve Yeager <steve.f.yeager@gmail.com>
2023-04-12 20:27:55 -0700

    Add IQueryable.IsEmpty extension method

--- LittleByte.Common/Database/Filter.cs
+++ LittleByte.Common/Database/Filter.cs
@@ -1,32 +1,35 @@
-﻿namespace LittleByte.Common.Database
-{
-    public static class IQueryableExtension
-    {
-        public static async Task<IQueryable<TModel>> FilterAsync<TModel, TRequest>(this IQueryable<TModel> query, Filter<TModel, TRequest> filter, TRequest request)
-        {
-            query = await filter.FilterAsync(query, request);
-            return query;
-        }
+﻿namespace LittleByte.Common.Database;
 
-        public static async Task<IQueryable<TModel>> FilterAsync<TModel, TRequest>(this Task<IQueryable<TModel>> queryTask, Filter<TModel, TRequest> filter, TRequest request)
-        {
-            var query = await queryTask;
-            query = await filter.FilterAsync(query, request);
-            return query;
-        }
+public static class IQueryableExtension
+{
+    public static async Task<IQueryable<TModel>> FilterAsync<TModel, TRequest>(
+        this IQueryable<TModel> @this, Filter<TModel, TRequest> filter, TRequest request)
+    {
+        @this = await filter.FilterAsync(@this, request);
+        return @this;
     }
 
-    public abstract class Filter<TModel, TRequest>
+    public static async Task<IQueryable<TModel>> FilterAsync<TModel, TRequest>(
+        this Task<IQueryable<TModel>> @this, Filter<TModel, TRequest> filter, TRequest request)
     {
-        protected abstract bool WillFilter(TRequest request);
-        protected abstract Task<IQueryable<TModel>> FilterAsyncInternal(IQueryable<TModel> query, TRequest request);
+        var query = await @this;
+        query = await filter.FilterAsync(query, request);
+        return query;
+    }
 
-        public async Task<IQueryable<TModel>> FilterAsync(IQueryable<TModel> query, TRequest request)
-        {
-            var willFilter = WillFilter(request);
+    public static bool IsEmpty<T>(this IQueryable<T> queryable) => !queryable.Any();
+}
 
-            Task<IQueryable<TModel>> task = willFilter ? FilterAsyncInternal(query, request) : Task.FromResult(query);
-            return await task;
-        }
+public abstract class Filter<TModel, TRequest>
+{
+    protected abstract bool WillFilter(TRequest request);
+    protected abstract Task<IQueryable<TModel>> FilterAsyncInternal(IQueryable<TModel> query, TRequest request);
+
+    public async Task<IQueryable<TModel>> FilterAsync(IQueryable<TModel> query, TRequest request)
+    {
+        var willFilter = WillFilter(request);
+
+        var task = willFilter ? FilterAsyncInternal(query, request) : Task.FromResult(query);
+        return await task;
     }
 }
