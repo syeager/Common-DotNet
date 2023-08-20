5ea748555b30ff41300cb5d2405204299818f2b7
Steve Yeager <steve.f.yeager@gmail.com>
2023-08-20 09:24:01 -0700

    Change Routes.Create and .GetById to be a more standard format

--- LittleByte.Common/AspNet/Core/Routes.cs
+++ LittleByte.Common/AspNet/Core/Routes.cs
@@ -5,8 +5,8 @@ namespace LittleByte.Common.AspNet.Core;
 [SuppressMessage("ReSharper", "PartialTypeWithSinglePart")]
 public static partial class Routes
 {
-    public const string Create = "create";
-    public const string GetById = "get-id";
+    public const string Create = "";
+    public const string GetById = "";
     public const string GetByPage = "get-page";
     public const string LogIn = "log-in";
 }
