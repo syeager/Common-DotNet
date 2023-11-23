a4c3f9020b6114a4c31a22a4aa8c4b079794da3a
Steve <steve.f.yeager@gmail.com>
2023-11-23 11:17:29 -0800

    Move Controller

---
+++ LittleByte.AspNet/Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LittleByte.AspNet;
+
+[ApiController]
+[Authorize]
+[Route("[controller]")]
+public abstract class Controller : ControllerBase
+{
+    public Guid? UserId => HttpContext.GetUserId();
+}
