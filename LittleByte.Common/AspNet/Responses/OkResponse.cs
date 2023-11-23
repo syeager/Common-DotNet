eb8af05c22b06c6c814ea3ead2f3de82d3ac0a9d
Steve Yeager <steve.f.yeager@gmail.com>
2023-11-23 08:53:33 -0800

    Add configuration for MVC DTO validation error handling (#5)
    * Add configuration for MVC DTO validation error handling
    
    * Make ApiResponse properties required
    
    * Check for validation errors being null
    
    * Return unauthorized ApiResponse if JWTR challenge fails

--- LittleByte.Common/AspNet/Responses/OkResponse.cs
+++ LittleByte.Common/AspNet/Responses/OkResponse.cs
@@ -4,12 +4,12 @@ namespace LittleByte.Common.AspNet.Responses;
 
 public class OkResponse : ApiResponse
 {
-    public OkResponse(string? message = null)
+    public OkResponse(string message = "")
         : base(HttpStatusCode.OK, message) { }
 }
 
 public class OkResponse<T> : ApiResponse<T> where T : class
 {
-    public OkResponse(T obj, string? message = null)
+    public OkResponse(T obj, string message = "")
         : base(HttpStatusCode.OK, obj, message) { }
 }
