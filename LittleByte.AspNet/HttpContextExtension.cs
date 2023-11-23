d74e201a1b14283b7f1093444bb88ace27881d93
Steve <steve.f.yeager@gmail.com>
2023-11-23 11:11:20 -0800

    Create LittleByte.AspNet

---
+++ LittleByte.AspNet/HttpContextExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+namespace LittleByte.AspNet;
+
+public static class HttpContextExtension
+{
+    public static Guid? GetUserId(this HttpContext httpContext)
+    {
+        var claim = httpContext.User?.Claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.NameId);
+        return claim == null ? null : Guid.Parse(claim.Value);
+    }
+}
---
+++ LittleByte.AspNet/OkResponse.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace LittleByte.AspNet;
+
+public class OkResponse : ApiResponse
+{
+    public OkResponse(string message = "")
+        : base(HttpStatusCode.OK, message) { }
+}
+
+public class OkResponse<T> : ApiResponse<T>
+    where T : class
+{
+    public OkResponse(T obj, string message = "")
+        : base(HttpStatusCode.OK, obj, message) { }
+}
