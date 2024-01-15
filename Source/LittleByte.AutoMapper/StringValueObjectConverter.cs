198a7db68e1526ed610468ec21613c115ec85ada
Steve Yeager <steve.f.yeager@gmail.com>
2024-01-15 12:29:37 -0800

    Mark as UsedImplicitly

--- Source/LittleByte.AutoMapper/StringValueObjectConverter.cs
+++ Source/LittleByte.AutoMapper/StringValueObjectConverter.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
+using JetBrains.Annotations;
 using LittleByte.Common;
 
 namespace LittleByte.AutoMapper;
 
+[UsedImplicitly]
 public sealed class StringValueObjectConverter : ITypeConverter<StringValueObject, string>
 {
     public string Convert(StringValueObject source, string destination, ResolutionContext context) => source.Value;
