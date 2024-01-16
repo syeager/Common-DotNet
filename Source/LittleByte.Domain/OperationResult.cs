e7cdb409522bf7a345a48824aea10681ca510068
Steve Yeager <steve.f.yeager@gmail.com>
2024-01-16 15:08:30 -0800

    Make OperationResult a record

--- Source/LittleByte.Domain/OperationResult.cs
+++ Source/LittleByte.Domain/OperationResult.cs
@@ -1,20 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace LittleByte.Domain
+﻿namespace LittleByte.Domain
 {
-    public class OperationResult(bool succeeded, string? errorMessage = null)
-    {
-        [MemberNotNullWhen(false, nameof(ErrorMessage))]
-        public bool Succeeded { get; } = succeeded;
-
-        public string? ErrorMessage { get; } = errorMessage;
-
-        public string Type => GetType().Name;
-
-        public void Deconstruct(out bool succeeded, out string? errorMessage)
-        {
-            succeeded = Succeeded;
-            errorMessage = ErrorMessage;
-        }
-    }
+    public record OperationResult(bool Succeeded, string ErrorMessage = "");
 }
