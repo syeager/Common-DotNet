210aede32b0a93e4a26f78432bc3a05f6e105cc0
Steve Yeager <steve.f.yeager@gmail.com>
2024-01-24 18:52:16 -0800

    Add EF ValueConverter for Ids and ModelBuilder helper for DomainModels

---
+++ Source/LittleByte.EntityFramework/IdValueConverter.cs
@@ -0,0 +1,17 @@
+﻿using LittleByte.Common;
+using LittleByte.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace LittleByte.EntityFramework;
+
+public sealed class IdValueConverter<T>() : ValueConverter<Id<T>, Guid>(id => id.Value, guid => new Id<T>(guid));
+
+public static class IdValueEntity
+{
+    public static void IdEntity<T>(this ModelBuilder builder)
+    {
+        builder.Entity<DomainModel<T>>().HasKey(dm => dm.Id);
+        builder.Entity<DomainModel<T>>().Property(dm => dm.Id).HasConversion<IdValueConverter<T>>();
+    }
+}
