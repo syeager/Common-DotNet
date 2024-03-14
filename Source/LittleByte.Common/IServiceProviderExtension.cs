6a68b14bc577a2c8675a415014cf0deb737cd4c8
Steve Yeager <steve.f.yeager@gmail.com>
2024-03-13 18:34:27 -0700

    Add IServiceProvider .Create extension method

--- Source/LittleByte.Common/IServiceProviderExtension.cs
+++ Source/LittleByte.Common/IServiceProviderExtension.cs
@@ -6,4 +6,20 @@ public static class IServiceProviderExtension
 {
     public static T Get<T>(this IServiceProvider @this)
         where T : notnull => @this.GetRequiredService<T>();
+
+    public static T Create<T>(this IServiceProvider @this)
+        where T : notnull
+    {
+        var constructor = typeof(T).GetConstructors().First();
+        var args = new object[constructor.GetParameters().Length];
+        for(var i = 0; i < args.Length; i++)
+        {
+            var param = constructor.GetParameters()[i];
+            var instance = @this.GetRequiredService(param.ParameterType);
+            args[i] = instance;
+        }
+
+        var obj = constructor.Invoke(args);
+        return (T)obj;
+    }
 }
