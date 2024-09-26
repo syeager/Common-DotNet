7e11947f69f322001dcbfefcb88b468b0c90a1d7
Steve Yeager <steve.f.yeager@gmail.com>
2024-09-25 22:12:28 -0700

    Add non-generic NoAwait() for Value/Task

--- Source/LittleByte.Common/TaskExtension.cs
+++ Source/LittleByte.Common/TaskExtension.cs
@@ -6,4 +6,7 @@ public static class TaskExtension
 {
     public static ConfiguredTaskAwaitable<T> NoAwait<T>(this Task<T> @this)
         => @this.ConfigureAwait(false);
+
+    public static ConfiguredTaskAwaitable NoAwait(this Task @this)
+        => @this.ConfigureAwait(false);
 }
--- Source/LittleByte.Common/ValueTaskExtension.cs
+++ Source/LittleByte.Common/ValueTaskExtension.cs
@@ -6,4 +6,7 @@ public static class ValueTaskExtension
 {
     public static ConfiguredValueTaskAwaitable<T> NoAwait<T>(this ValueTask<T> @this)
         => @this.ConfigureAwait(false);
+
+    public static ConfiguredValueTaskAwaitable NoAwait(this ValueTask @this)
+        => @this.ConfigureAwait(false);
 }
