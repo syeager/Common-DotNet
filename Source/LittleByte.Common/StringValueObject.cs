430cbf530d555c5f29331fad98afeea97feb0ba8
Steve Yeager <steve.f.yeager@gmail.com>
2024-09-25 22:12:05 -0700

    Add Length property to StingValueObject

--- Source/LittleByte.Common/StringValueObject.cs
+++ Source/LittleByte.Common/StringValueObject.cs
@@ -2,6 +2,7 @@
 
 public abstract record StringValueObject(string Value)
 {
+    public int Length => Value.Length;
     public override string ToString() => Value;
 
     public static implicit operator string(StringValueObject @this) => @this.Value;
