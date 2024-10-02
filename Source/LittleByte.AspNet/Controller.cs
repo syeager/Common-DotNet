9208550dda61cd620bb9724ba84f9fc81a0cfc69
Steve Yeager <steve.f.yeager@gmail.com>
2024-10-01 21:19:53 -0700

    Add Controller.Ok

--- Source/LittleByte.AspNet/Controller.cs
+++ Source/LittleByte.AspNet/Controller.cs
@@ -5,8 +5,10 @@ namespace LittleByte.AspNet;
 
 [ApiController]
 [Authorize]
-[Route("[controller]")]
 public abstract class Controller : ControllerBase
 {
     public Guid? UserId => HttpContext.GetUserId();
+
+    protected static OkResponse Ok(string message = "") => new(message);
+    protected static OkResponse<T> Ok<T>(T obj, string message = "") where T : class => new(obj, message);
 }
