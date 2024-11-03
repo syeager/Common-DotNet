93a8fea1a2d9363a8e295f9dad912b6e7e1fe8ac
Steve Yeager <steve.f.yeager@gmail.com>
2024-11-03 13:14:53 -0800

    Add GetHashCode to StringValueObject

--- Source/LittleByte.Common/StringValueObject.cs
+++ Source/LittleByte.Common/StringValueObject.cs
@@ -3,7 +3,10 @@
 public abstract record StringValueObject(string Value)
 {
     public int Length => Value.Length;
+
     public override string ToString() => Value;
+    public override int GetHashCode() => Value.GetHashCode();
+
 
     public static implicit operator string(StringValueObject @this) => @this.Value;
 }
